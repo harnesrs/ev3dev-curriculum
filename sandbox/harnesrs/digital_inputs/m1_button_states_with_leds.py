9f3e6141c4920ad4307b5638ef2457b4a947ba4e
harnesrs <harnesrs@rose-hulman.edu>
2017-10-26 14:54:56 -0400

    commit

--- sandbox/harnesrs/digital_inputs/m1_button_states_with_leds.py
+++ sandbox/harnesrs/digital_inputs/m1_button_states_with_leds.py
@@ -22,34 +22,34 @@ def main():
     print("--------------------------------------------")
     ev3.Sound.speak("Buttons and L E Dees").wait()
 
-    # Opening LED dance (to show the LED syntax)
-    # Red LEDs
-    ev3.Sound.speak("Red")
-    ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.RED)
-    ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.RED)
-    time.sleep(3)
-
-    # Green LEDs
-    ev3.Sound.speak("Green")
-    ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.GREEN)
-    ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.GREEN)
-    time.sleep(3)
-
-    # Turn LEDs off
-    ev3.Sound.speak("Off")
-    ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.BLACK)
-    ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.BLACK)
-    # ev3.Leds.all_off()  # Could also use this single command if turning both LEDs off.
-    print('Press the Back button on the EV3 to exit this program.')
-
-    # Buttons on EV3 (the real focus of this module)
-    btn = ev3.Button()  # Construct the one and only EV3 Button object
-    led_colors = [ev3.Leds.BLACK,  # This list is useful for the down button in TO DO 4.
-                  ev3.Leds.GREEN,
-                  ev3.Leds.RED,
-                  # ev3.Leds.ORANGE,  # Too close to another color in my opinion
-                  # ev3.Leds.YELLOW,  # Too close to another color in my opinion
-                  ev3.Leds.AMBER]
+    # # Opening LED dance (to show the LED syntax)
+    # # Red LEDs
+    # ev3.Sound.speak("Red")
+    # ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.RED)
+    # ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.RED)
+    # time.sleep(3)
+    #
+    # # Green LEDs
+    # ev3.Sound.speak("Green")
+    # ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.GREEN)
+    # ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.GREEN)
+    # time.sleep(3)
+    #
+    # # Turn LEDs off
+    # ev3.Sound.speak("Off")
+    # ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.BLACK)
+    # ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.BLACK)
+    # # ev3.Leds.all_off()  # Could also use this single command if turning both LEDs off.
+    # print('Press the Back button on the EV3 to exit this program.')
+    #
+    # # Buttons on EV3 (the real focus of this module)
+    # btn = ev3.Button()  # Construct the one and only EV3 Button object
+    # led_colors = [ev3.Leds.BLACK,  # This list is useful for the down button in TO DO 4.
+    #               ev3.Leds.GREEN,
+    #               ev3.Leds.RED,
+    #               # ev3.Leds.ORANGE,  # Too close to another color in my opinion
+    #               # ev3.Leds.YELLOW,  # Too close to another color in my opinion
+    #               ev3.Leds.AMBER]
 
     current_color_index = 0
     while True:
@@ -70,6 +70,17 @@ def main():
         #   Optional: You can also comment out the code above that does the 6 second red, green, off pattern.  It was
         #     there just to provide you with code examples for using the LEDs.  It does not need to run anymore.
         #     Just make sure not to comment out too much. ;)
+        if btn.up:
+            print('up')
+            ev3.Leds.all_off()
+        if btn.left:
+            print('left')
+            ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.GREEN)
+            ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.BLACK)
+        if btn.right:
+            print('right')
+            ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.RED)
+            ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.BLACK)
 
         # TODO: 4. Implement the down button to change the color of both LEDs.
         #   The first press to down should make both LEDs GREEN, the next press makes them RED, then AMBER, then off.

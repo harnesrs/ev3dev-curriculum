137b306e988dc4a86b3a7ce56036be33b2520af2
kaiserar <kaiserar@rose-hulman.edu>
2017-10-30 14:56:14 -0400

    done

--- sandbox/kaiserar/digital_inputs/m4_touch_sensor_arm_movement.py
+++ sandbox/kaiserar/digital_inputs/m4_touch_sensor_arm_movement.py
@@ -77,7 +77,7 @@ def arm_calibration(arm_motor, touch_sensor):
     while touch_sensor.is_pressed == False:
         time.sleep(0.01)
     arm_motor.stop(stop_action="brake")
-    ev3.Sound.beep().wait()
+    ev3.Sound.beep().wait() 
 
     arm_revolutions_for_full_range = 14.2 * 360
     arm_motor.run_to_rel_pos(position_sp=-arm_revolutions_for_full_range)

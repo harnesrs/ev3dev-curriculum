5a03f83db2dceb8c064a40ff691b3258613d8904
kaiserar <kaiserar@rose-hulman.edu>
2017-10-31 12:16:36 -0400

    done

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -70,3 +70,6 @@ class Snatch3r(object):
         self.arm_motor.run_to_abs_pos(position_sp=0, speed_sp=900)
         self.arm_motor.wait_while(ev3.Motor.STATE_RUNNING)  # Blocks until the motor finishes running
         ev3.Sound.beep().wait()
+
+    def shutdown(self):
+        

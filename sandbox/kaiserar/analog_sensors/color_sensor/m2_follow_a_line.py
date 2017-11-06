7b75b3e51d8dbb071e344ff732a85eb546089039
kaiserar <kaiserar@rose-hulman.edu>
2017-11-06 15:16:22 -0500

    done

--- sandbox/kaiserar/analog_sensors/color_sensor/m2_follow_a_line.py
+++ sandbox/kaiserar/analog_sensors/color_sensor/m2_follow_a_line.py
@@ -80,9 +80,9 @@ def follow_the_line(robot, white_level, black_level):
     # should drive straight or turn to the right.  You will need to test and refine your code until it works well.
     # Optional extra - For a harder challenge could you drive on the black line and handle left or right turns?
     while True:
-        while robot.color_sensor.reflected_light_intensity <= black_level:
+        while robot.color_sensor.reflected_light_intensity <= black_level+5:
             robot.forward_drive(300, 300)
-        while robot.color_sensor.reflected_light_intensity >= white_level:
+        while robot.color_sensor.reflected_light_intensity >= white_level-5:
             robot.left_drive(300)
         if robot.touch_sensor.is_pressed:
             break

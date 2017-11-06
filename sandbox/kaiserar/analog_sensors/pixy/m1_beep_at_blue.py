d4f1d4309ac581f50bc4f3d24e3b6e7873dfd0cc
kaiserar <kaiserar@rose-hulman.edu>
2017-11-05 22:03:22 -0500

    done

--- sandbox/kaiserar/analog_sensors/pixy/m1_beep_at_blue.py
+++ sandbox/kaiserar/analog_sensors/pixy/m1_beep_at_blue.py
@@ -36,7 +36,7 @@ def main():
         #   assert self.pixy
         # Then here you can use a command like width = robot.pixy.value(3)
         print("(X,Y) = ({}, {})  Width = {}  Height = {}".format(robot.pixy.value(1), robot.pixy.value(2), robot.pixy.value(3), robot.pixy.value(4)))
-        if robot.pixy.value3 > 0:
+        while robot.pixy.value(3) > 0:
             ev3.Sound.beep()
             time.sleep(1.5)
 

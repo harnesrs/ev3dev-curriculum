220f28a554be052b5f06dabeb11503b150a426d5
kaiserar <kaiserar@rose-hulman.edu>
2017-11-09 13:22:02 -0500

    done

--- projects/kaiserar/cp_code.py
+++ projects/kaiserar/cp_code.py
@@ -25,8 +25,12 @@ def tkinter_window():
     bbutton.grid(row=2, column=1)
     slabel = ttk.Label(frame1, text = "Speed:")
     slabel.grid(row=3, column=0)
-    sbox = ttk.Entry(frame1)
+    sbox = ttk.Scale(frame1, from_=0, to=800)
     sbox.grid(row=3, column=1)
+    sread = ttk.Label(frame1, text="{}".format(sbox.get()))
+    sread.grid(row=3, column=2)
+    mlabel = ttk.Label(frame1, text = "")
+    mlabel.grid(row=4, column=1)
     root.mainloop()
 
 def mqtt_connect():
@@ -37,7 +41,7 @@ def mqtt_connect():
 class Delegate(object):
     def __init__(self):
         pass
-    def display_message(self, root):
+    def display_message(self, mlabel):
         pass
 
 
@@ -45,5 +49,35 @@ class Delegate(object):
 tkinter_window()
 
 
+def main():
+    # tkinter construction
+    root = tkinter.Tk()
+    root.title("Controller")
+    frame1 = ttk.Frame(root, padding=30)
+    frame1.grid()
+    fbutton = ttk.Button(frame1, text="Forward")
+    fbutton.grid(row=0, column=1)
+    lbutton = ttk.Button(frame1, text="Left Turn")
+    lbutton.grid(row=1, column=0)
+    sbutton = ttk.Button(frame1, text="Brake")
+    sbutton.grid(row=1, column=1)
+    rbutton = ttk.Button(frame1, text="Right Turn")
+    rbutton.grid(row=1, column=2)
+    bbutton = ttk.Button(frame1, text="Reverse")
+    bbutton.grid(row=2, column=1)
+    slabel = ttk.Label(frame1, text="Speed:")
+    slabel.grid(row=3, column=0)
+    sbox = ttk.Scale(frame1, from_=0, to=800)
+    sbox.grid(row=3, column=1)
+    sread = ttk.Label(frame1, text="{}".format(sbox.get()))
+    sread.grid(row=3, column=2)
+    mlabel = ttk.Label(frame1, text="")
+    mlabel.grid(row=4, column=1)
+    root.mainloop()
 
+    # mqtt connect
+    delegate = Delegate()
+    client = com.MqttClient(delegate)
+    client.connect_to_ev3()
 
+    

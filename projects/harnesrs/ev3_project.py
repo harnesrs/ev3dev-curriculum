cca1fc41153cf68a96e7a9cf84181b480db67f41
harnesrs <harnesrs@rose-hulman.edu>
2017-11-11 14:25:48 -0500

    commit

--- projects/harnesrs/ev3_project.py
+++ projects/harnesrs/ev3_project.py
@@ -10,33 +10,32 @@ import time
 import mqtt_remote_method_calls as com
 
 robot = robo.Snatch3r()
-left_color = 5
-right_color = 2
 
 class MyDelegate(object):
 
     def __init__(self):
         self.direction = 0
         self.drive = False
+        self.left_color = 5
+        self.right_color = 2
+        self.direction_list = ['forward', 'right', 'backward', 'left']
 
     def drive_button(self):
         robot.forward_drive(300, 300)
         self.drive = True
-        ev3.Sound.speak('Driving').wait()
 
     def stop_button(self):
         robot.stop()
         self.drive = False
-        ev3.Sound.speak('Stopping').wait()
 
-    def left_color(self, color):
-        left_color = color
+    def set_left_color(self, color):
+        self.left_color = color
 
-    def right_color(self, color):
-        right_color = color
+    def set_right_color(self, color):
+        self.right_color = color
 
-    def direction(self):
-        return self.direction
+    def find_direction(self):
+        print('I am facing', self.direction_list[self.direction])
 
 
 def main():
@@ -53,28 +52,29 @@ def main():
         if btn.up:
             print('Up button')
             handle_up_button(mqtt_client, my_delegate)
-            time.sleep(3)
+            time.sleep(1)
         if btn.down:
             print('Down button')
             handle_down_button(mqtt_client, my_delegate)
-            time.sleep(3)
+            time.sleep(1)
         if btn.right:
             print('Right button')
             handle_right_button(mqtt_client, my_delegate)
-            time.sleep(3)
+            time.sleep(1)
         if btn.left:
             print('Left button')
             handle_left_button(mqtt_client, my_delegate)
-            time.sleep(3)
+            time.sleep(1)
 
-        if robot.color_sensor.color == left_color:
+        if robot.color_sensor.color == my_delegate.left_color:
+            # print('left color is:', my_delegate.left_color)
             print('Turning left')
             turn_left(mqtt_client, my_delegate)
-            time.sleep(3)
-        if robot.color_sensor.color == right_color:
+
+        if robot.color_sensor.color == my_delegate.right_color:
+            # print('right color is:', my_delegate.right_color)
             print('Turning right')
             turn_right(mqtt_client, my_delegate)
-            time.sleep(3)
 
         if btn.backspace:
             print('Backspace')
@@ -83,44 +83,50 @@ def main():
 
 def handle_up_button(mqtt_client, my_delegate):
     mqtt_client.send_message('turn_forward')
-    degrees = (my_delegate.direction * 90) % 360
-    robot.turn_degrees(-degrees, 300)
+    degrees = (my_delegate.direction * 90)
+    robot.turn_degrees(degrees, 300)
     my_delegate.direction = 0
     if my_delegate.drive:
         robot.forward_drive(300, 300)
 
 def handle_down_button(mqtt_client, my_delegate):
     mqtt_client.send_message('turn_backward')
-    degrees = (my_delegate.direction * 90) % 360 + 180
-    robot.turn_degrees(-degrees, 300)
+    degrees = (my_delegate.direction * 90) + 180
+    robot.turn_degrees(degrees, 300)
     my_delegate.direction = 2
     if my_delegate.drive:
         robot.forward_drive(300, 300)
 
 def handle_right_button(mqtt_client, my_delegate):
     mqtt_client.send_message('turn_right')
-    degrees = (my_delegate.direction * 90) % 360 + 90
-    robot.turn_degrees(-degrees, 300)
+    degrees = (my_delegate.direction * 90) - 90
+    robot.turn_degrees(degrees, 300)
     my_delegate.direction = 1
     if my_delegate.drive:
         robot.forward_drive(300, 300)
 
 def handle_left_button(mqtt_client, my_delegate):
+    # print('before left button direction is:', my_delegate.direction)
     mqtt_client.send_message('turn_left')
-    degrees = (my_delegate.direction * 90) % 360 + 270
-    robot.turn_degrees(-degrees, 300)
+    degrees = (my_delegate.direction * 90) + 90
+    robot.turn_degrees(degrees, 300)
     my_delegate.direction = 3
+    # print('after left button direction is:', my_delegate.direction)
     if my_delegate.drive:
         robot.forward_drive(300, 300)
 
 def turn_left(mqtt_client, my_delegate):
     if my_delegate.drive:
+        # print('before turning left direction is:', my_delegate.direction)
+        my_delegate.direction = (my_delegate.direction + 3) % 4
         robot.turn_degrees(90, 300)
-        my_delegate.direction = my_delegate.direction + 1
+        # print('after turning left direction is:', my_delegate.direction)
+        robot.forward_drive(300, 300)
 
 def turn_right(mqtt_client, my_delegate):
     if my_delegate.drive:
+        my_delegate.direction = (my_delegate.direction + 1) % 4
         robot.turn_degrees(-90, 300)
-        my_delegate.direction = my_delegate.direction - 1
+        robot.forward_drive(300, 300)
 
 main()
--- projects/harnesrs/pc_project.py
+++ projects/harnesrs/pc_project.py
@@ -25,7 +25,6 @@ def main():
     mqtt_client.connect_to_ev3()
 
     color_list = [0, 1, 'blue', 'green', 4, 'red']
-    direction_list = ['forward', 'right', 'backward', 'left']
 
     root = tkinter.Tk()
     root.title('Color')
@@ -38,15 +37,13 @@ def main():
 
     red_left = ttk.Radiobutton(main_frame, text='Red', variable = left, value = 5)
     red_left.grid(row=0, column=0)
+    red_left.invoke()
 
     blue_left = ttk.Radiobutton(main_frame, text='Blue', variable = left, value = 2)
     blue_left.grid(row=0, column=1)
 
     green_left = ttk.Radiobutton(main_frame, text='Green', variable = left, value = 3)
     green_left.grid(row=0, column=2)
-
-    left_color = ttk.Label(main_frame, text = 'turn left color')
-    left_color.grid(row = 1, column = 1)
 
     set_left_color = ttk.Button(main_frame, text = 'Set left color')
     set_left_color.grid(row = 2, column = 1)
@@ -57,12 +54,10 @@ def main():
 
     blue_right = ttk.Radiobutton(main_frame, text='Blue', variable = right, value = 2)
     blue_right.grid(row=0, column=5)
+    blue_right.invoke()
 
     green_right = ttk.Radiobutton(main_frame, text='Green', variable =  right, value = 3)
     green_right.grid(row=0, column=6)
-
-    right_color = ttk.Label(main_frame, text = 'turn right color')
-    right_color.grid(row = 1, column = 5)
 
     set_right_color = ttk.Button(main_frame, text = 'Set right color')
     set_right_color.grid(row = 2, column = 5)
@@ -78,13 +73,15 @@ def main():
 
     direction = ttk.Button(main_frame, text = 'Direction')
     direction.grid(row = 4, column = 3)
-    direction['command'] = lambda: direction_button(mqtt_client, direction_list)
+    direction['command'] = lambda: direction_button(mqtt_client)
 
     quit_button = ttk.Button(main_frame, text = 'Quit')
     quit_button.grid(row = 5, column = 2)
+    quit_button['command'] = lambda: quit_program(mqtt_client, False)
 
     exit_button = ttk.Button(main_frame, text = 'Exit')
     exit_button.grid(row = 5, column = 4)
+    exit_button['command'] = lambda: quit_program(mqtt_client, True)
 
     root.mainloop()
 
@@ -98,14 +95,20 @@ def handle_stop_button(mqtt_client):
 
 def set_left_color_button(mqtt_client, color, color_list):
     print('Turn left color is:', color_list[color])
-    mqtt_client.send_message('left_color', [color])
+    mqtt_client.send_message('set_left_color', [color])
 
 def set_right_color_button(mqtt_client, color, color_list):
     print('Turn right color is:', color_list[color])
-    mqtt_client.send_message('right_color', [color])
+    mqtt_client.send_message('set_right_color', [color])
 
-def direction_button(mqtt_client, direction_list):
-    direction = mqtt_client.send_message('direction')
-    print('I am facing', direction_list[direction])
+def direction_button(mqtt_client):
+    mqtt_client.send_message('find_direction', [])
+
+def quit_program(mqtt_client, shutdown_ev3):
+    if shutdown_ev3:
+        print("shutdown")
+        mqtt_client.send_message("shutdown")
+    mqtt_client.close()
+    exit()
 
 main()
